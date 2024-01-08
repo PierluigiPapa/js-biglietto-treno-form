f3b72d12054a702e7cf21ca4828c20fe27f6881a
PierluigiPapa <pierluigipapa9@gmail.com>
2024-01-08 16:17:23 +0100

    feat: add let on js

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,31 @@
+// MILESTONE 1:
+// Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console.
+
+// MILESTONE 2:
+// Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
+// Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
 //costante dei km percorsi
 const Km= document.getElementById('km')
 
 //costante dell'età per accedere o meno allo sconto
-const Age = document.getElementById('age')
+const Età = document.getElementById('età')
 
 //costante per il bottone da inserire nel form
 const Button = document.getElementById('button')
 
 //costante per generare il prezzo del biglietto
-const Price = document.getElementById('user')
+const prezzo = document.getElementById('prezzo')
+
+//variabile per generare il numero della carrozza
+let Carrozza = document.getElementById('carrozza')
+
+//variabile per riconoscere la tipologia dell'offerta
+let Offerta = document.getElementById('offerta')
+
+//variabile per generare il codice del biglietto
+let Biglietto = document.getElementById('ticket')
+
+
 
 
