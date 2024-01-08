d15de9e227f2f7b43c90f8ef0f20cd097a0fbde4
PierluigiPapa <pierluigipapa9@gmail.com>
2024-01-08 16:09:47 +0100

    feat: add title (h1), background colour (css) and costants (js)

---
+++ assets/main.js
@@ -0,0 +1,13 @@
+//costante dei km percorsi
+const Km= document.getElementById('km')
+
+//costante dell'età per accedere o meno allo sconto
+const Age = document.getElementById('age')
+
+//costante per il bottone da inserire nel form
+const Button = document.getElementById('button')
+
+//costante per generare il prezzo del biglietto
+const Price = document.getElementById('user')
+
+
