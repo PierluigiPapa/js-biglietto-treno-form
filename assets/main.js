4cb75b07534acb3a7bbdde7600e100eab3619f44
PierluigiPapa <pierluigipapa9@gmail.com>
2024-01-08 17:40:47 +0100

    feat: add if (js)

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,11 @@
+// Scrivere un programma che chieda all’utente:
+// - Il numero di chilometri da percorrere
+// - Età del passeggero
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+// - il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// - va applicato uno sconto del 20% per i minorenni
+// - va applicato uno sconto del 40% per gli over 65.
+
 // MILESTONE 1:
 // Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console.
 
@@ -5,27 +13,53 @@
 // Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
 // Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
+//costante per prezzo del biglietto
+const costoKm = 0.21;
+
+//costante per sconto Under18
+const Under18 = 0.2;
+
+//costante per sconto Over65
+const Over65 = 0.4;
+
 //costante dei km percorsi
-const Km= document.getElementById('km')
+const kmUtente= document.getElementById('km');
 
 //costante dell'età per accedere o meno allo sconto
-const Età = document.getElementById('età')
+const etaUtente = document.getElementById('eta');
 
-//costante per il bottone da inserire nel form
-const Button = document.getElementById('button')
+//costante per il bottone da inserire nel form per calcolare il prezzo del biglietto
+const submitPrice = document.getElementById('submit');
 
-//costante per generare il prezzo del biglietto
-const prezzo = document.getElementById('prezzo')
+//costante per il bottone di annullare 
+const deletePrice =  document.getElementById('delete')
 
-//variabile per generare il numero della carrozza
-let Carrozza = document.getElementById('carrozza')
+//costante per generare il nome e il cognome del passeggero
+const userPasseggero = document.getElementById('user');
 
-//variabile per riconoscere la tipologia dell'offerta
-let Offerta = document.getElementById('offerta')
+//costante per la creazione del biglietto virtuale
+const ticketVirtual = document.getElementById ('ticket')
 
-//variabile per generare il codice del biglietto
-let Biglietto = document.getElementById('ticket')
+//serie di costanti per gli elementi generati nel biglietto
+const userNomeCognome= document.querySelector ('nomecognome')
+const offerTrain = document.querySelector ('offerta')
+const sectionTrain = document.querySelector ('section')
+const codeTrain = document.querySelector ('code')
+const prezzoTrain = document.querySelector ('pay')
 
+//calcolo del biglietto
+submitPrice.addEventListener ( 'click', function () {
+    //prezzo del bigliettostandard //
+            let prezzoBiglietto = costoKm * Number(kmUtente.value);
+            let TypeTicket = 'Prezzo standard';
 
+            // prezzo del biglietto Under18
+            if etaUtente.value === 'minorenne') {
+                prezzoBiglietto = (prezzoBiglietto * Under18);
+                let TypeTicket = 'Prezzo Under18';
 
-
+            //prezzo del biglietto Over65
+            } else if (etaUtente.value === 'oltre65') {
+                prezzoBiglietto = (prezzoBiglietto * Over65);
+                let TypeTicket = 'Prezzo Over65';
+            }
